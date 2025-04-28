66a05a28845e13f94448ebfeafd99149f9ab9686
Foo <34515228+fooooooooooooooo@users.noreply.github.com>
2025-04-28 03:39:25 -0700

    log messages

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -8,6 +8,18 @@ const { data: page } = await useAsyncData('page-' + route.path, () => {
 if (!page.value) {
   throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
 }
+
+function logEvents(e: { data: unknown }) {
+  console.info('message:', e.data);
+}
+
+onMounted(() => {
+  window.addEventListener('message', logEvents);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('message', logEvents);
+});
 </script>
 
 <template>
